90111f59341f07d99fda771343e95fc1234b87ba
github username <github email address>
2019-05-20 14:05:21 -0400

    Done.

--- Gemfile
+++ Gemfile
@@ -31,11 +31,12 @@ gem 'faraday'
 # Dot-env
 gem 'dotenv-rails'
 
+gem 'rack_session_access'
+
 #Testing
 group :test do
   gem 'rspec-rails'
   gem 'capybara'
-  gem 'rack_session_access'
   gem 'webmock'
 end
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,9 @@ class ApplicationController < ActionController::Base
   private
 
     def authenticate_user
-      redirect_to "https://github.com/login/oauth/authorize?client_id=#{ENV['GITHUB_CLIENT']}&scope=repo" if !logged_in?
+      if !logged_in?
+        redirect_to "https://github.com/login/oauth/authorize?client_id=#{ENV['GITHUB_CLIENT']}&scope=repo"
+      end
     end
 
     def logged_in?
